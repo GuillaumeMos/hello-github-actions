53b8d775a9b27fe608991c93d1c8505b0789b245
Shadx00 <shadx00@gmail.com>
2021-05-05 18:01:07 +0200

    Update test.sh

--- action-a/test.sh
+++ action-a/test.sh
@@ -2,14 +2,14 @@
 
 @test "Should pass" {
 
- result="$(INPUT_MY_NAME='Mona' ./entrypoint.sh)"
+ result="$(INPUT_MY_NAME='Mona' ./action-a/entrypoint.sh)"
  
  [ $? -eq 0 ]
  
 }
 @test "This test is a bug" {
 
- result="$(INPUT_MY_NAME='aMona' ./entrypoint.sh)"
+ result="$(INPUT_MY_NAME='aMona' ./action-a/entrypoint.sh)"
  
  [ $? -eq 1 ]
 
